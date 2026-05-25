855b9966e161a67d1d9c57cab621dd550ee8c73a
viveklb <69708672+viveklb@users.noreply.github.com>
2026-05-25 17:24:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-//complete this code
-class Person {}
+// complete this code
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for name
+  set name(name) {
+    this._name = name;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
